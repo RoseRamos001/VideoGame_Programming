b3057c6a4a47812aee486926e24dfb468d1457bf
luisge12 <43939186+luisge12@users.noreply.github.com>
2024-08-12 17:00:51 -0400

    Add files via upload

---
+++ breakout/src/powerups/__init__.py
@@ -0,0 +1,16 @@
+"""
+ISPPJ1 2024
+Study Case: Breakout
+
+Author: Alejandro Mujica
+alejandro.j.mujic4@gmail.com
+
+This module contains all of the power ups.
+"""
+
+from src.powerups.PowerUp import PowerUp
+from src.powerups.TwoMoreBall import TwoMoreBall
+from src.powerups.StickyPaddle import StickyPaddle
+from src.powerups.ExplosiveBalls import ExplosiveBalls
+
+(PowerUp, TwoMoreBall, StickyPaddle, ExplosiveBalls)
